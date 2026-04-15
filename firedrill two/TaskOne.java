306a46b08718671e5c04feab196d5f3df74c7a8a
BIGDUKE004 <elijahmiracle2007@gmail.com>
2026-04-15 17:08:34 +0100

    firedrilltwo

---
+++ firedrill two/TaskOne.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+        public class TaskOne{
+                    public static void main(String[] args){
+Scanner userinput = new Scanner(System.in);
+int i = 0;
+while (i <= 10 ) {
+  System.out.print("enter a number");
+  i++;
+}
+}
+}
