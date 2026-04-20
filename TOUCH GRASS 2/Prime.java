b537a4f5e805d77514dc807eeb01a863de1daaa6
BIGDUKE004 <elijahmiracle2007@gmail.com>
2026-04-20 08:28:22 +0100

    second assignment

---
+++ TOUCH GRASS 2/Prime.java
@@ -0,0 +1,14 @@
+public class Prime{
+public static void main(String[] args){
+    System.out.print(isPrimeNumber(5));
+}
+    public static boolean isPrimeNumber(int number){
+
+if( number % number == 1){
+return true;
+ } else{
+return false;
+}
+     
+}
+}
