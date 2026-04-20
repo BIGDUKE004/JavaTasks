1708846cff2915f97eb6b7ef40bb2149ca9d9945
BIGDUKE004 <elijahmiracle2007@gmail.com>
2026-04-20 08:27:38 +0100

    second assignment

---
+++ TOUCH GRASS 2/TrueOrFalse.java
@@ -0,0 +1,14 @@
+public class TrueOrFalse{
+public static void main(String[] args){
+    System.out.print(isEven(5));
+}
+    public static boolean isEven(int number){
+
+if( number % 2 == 0){
+return true;
+ } else{
+return false;
+}
+     
+}
+}
