f4fb447ebf6704e59bfc36c051f48c6d1853837a
BIGDUKE004 <elijahmiracle2007@gmail.com>
2026-05-02 12:53:28 +0100

    MENSTRUAL APP

---
+++ MENSTRUAL APP/MenstrualApp.java
@@ -0,0 +1,32 @@
+import java.time.LocalDateTime;;
+public class MenstrualApp{
+static int flow = 28;
+int flowtwo = 35;
+int  cycleLength =14;
+
+    public static boolean registration (String name, String email, String password ){
+ return true;
+
+}
+
+    public static int getCycleLength(int flowdate){
+int date = flow + flowdate;
+return date;
+}
+
+    public static int isCycleLengthTwo(int isCycleLengthTwo){
+//flow date 
+return 35;
+}
+
+public static int OvulationPeriod(int OvulationPeriod){
+return 14;
+}
+
+
+public static void main(String[] args){
+    System.out.println(getCycleLength(12));
+}
+}
+
+
